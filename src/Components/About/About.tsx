0e5f703c8ccc7ba65bdb86fb1ce6d9dc917089ca
Kunal Prajapati <kunalprajpati@gmail.com>
2025-02-06 22:22:58 +0530

    updated resume link

--- src/Components/About/About.tsx
+++ src/Components/About/About.tsx
@@ -40,7 +40,7 @@ const Profile: React.FC = () => {
                     <button
                         className="py-2 font-semibold text-white duration-500 bg-pink-600 border border-pink-600 rounded-md hover:text-pink-600 hover:border-pink-600 hover:bg-transparent text-main w-44"
                         onClick={() => {
-                            window.open('https://drive.google.com/file/d/1Mh_kHBJb_kqAnHmdvkEUXyaCSBDAAMZ8/view?usp=sharing', '_blank');
+                            window.open('https://drive.google.com/file/d/1HQ7lQpXF_u31aPyovItMsWp9FLfCYEyx/view?usp=sharing', '_blank');
                         }}
                     >
                         Resume
