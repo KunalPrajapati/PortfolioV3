d3ffde8cf04ea7622bb493c0f91e15a037890e15
Kunal Prajapati <40620699+KunalPrajapati@users.noreply.github.com>
2025-07-26 23:16:15 +0530

    resume link updated

--- src/Components/About/About.tsx
+++ src/Components/About/About.tsx
@@ -40,7 +40,7 @@ const Profile: React.FC = () => {
                     <button
                         className="py-2 font-semibold text-white duration-500 bg-pink-600 border border-pink-600 rounded-md hover:text-pink-600 hover:border-pink-600 hover:bg-transparent text-main w-44"
                         onClick={() => {
-                            window.open('https://drive.google.com/file/d/1Q8BGhfKjocQTKNopMfg1o0HuT6Mccbg5/view?usp=sharing', '_blank');
+                            window.open('https://drive.google.com/file/d/1mDwDxDFybzhZn789_Mysxr1wAEA6GVpU/view?usp=sharing', '_blank');
                         }}
                     >
                         Resume
